7a0106142968c8750f92fc0c779c830b122ef2b6
Girijesh1998 <mukeshtripathi752@gmail.com>
2024-04-15 13:24:16 +0530

    'hdjfh'

--- mycvextracter/settings.py
+++ mycvextracter/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'SECURTY_KEY'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
-ALLOWED_HOSTS = [ "*",'mycvextracter.herokuapp.com']
+ALLOWED_HOSTS = ['mycvextracter.herokuapp.com','localhost']
 
 
 # Application definition
